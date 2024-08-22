72eb4af291e66ec984a58dd327ad13ae6c1410ea
Marcos Bueno <marcos_vbp@outlook.com>
2024-08-22 17:48:37 -0300

    feat: add InputIconText component

--- src/Frontend/Pages/Home.tsx
+++ src/Frontend/Pages/Home.tsx
@@ -1,10 +1,20 @@
+import { PiPaperPlaneTilt } from 'react-icons/pi'
 import Footer from '../Components/Footer'
 import Header from '../Components/Header'
+import InputIconText from '../Components/InputIconText'
 
 const Home = () => {
   return (
     <>
       <Header />
+      <br/>
+      <InputIconText 
+        icon={<PiPaperPlaneTilt size={20} color='#A9AFBB' />} 
+        title='Destination' 
+        placeHolder='Destination' 
+        typeOperation='text'
+      />
+      <br/>
       <h1>Home</h1>
       <img src='https://desafio-3-mvbp-bucket.s3.amazonaws.com/Baloes.jpg' />
       <Footer />

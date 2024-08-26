d95bc0e2e498208d00805114d698f11c4adec334
Marcos Bueno <marcos_vbp@outlook.com>
2024-08-26 11:40:47 -0300

    feat: add reviews stats

--- src/Frontend/Pages/TuorView.tsx
+++ src/Frontend/Pages/TuorView.tsx
@@ -5,14 +5,33 @@ import Header from '../Components/Header'
 import MapComponent from '../Components/MapComponent'
 import global from './global.module.css'
 import style from './tuorView.module.css'
+import AverageReviews from '../Components/AverageReviews'
+import { FaStar } from 'react-icons/fa'
 
 const TuorView = () => {
+  const [services, setServices] = useState<number>(5)
+  const [location, setLocation] = useState<number>(5)
+  const [amenities, setAmenities] = useState<number>(5)
+  const [prices, setPrices] = useState<number>(4)
+  const [food, setFood] = useState<number>(3)
+  const [quality, setQuality] = useState<number>(5)
   const [name, setName] = useState<string>('anonymous')
   const [email, setEmail] = useState<string>('')
   const [comment, setComment] = useState<string>('No comment.')
 
   const lat: number = -23.187049672867087
   const lng: number = -50.65637960242221
+  const avgScore: number = (services + location + amenities + prices + quality) / 5
+  const iconSize: number = 20
+  let avgQuality: string
+
+  if(avgScore >= 4) {
+    avgQuality = 'Excellent'
+  }else if (avgScore <= 2) {
+    avgQuality = 'Bad'
+  }else{
+    avgQuality = 'Medium'
+  }
 
   function mostraReview (e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -35,6 +54,25 @@ const TuorView = () => {
             <MapComponent lat={lat} lng={lng}/>
 
             <h2 className={`blueText`}>Average Reviews</h2>
+            <div className={style.averageBox}>
+              <div className={style.averageScore}>
+                <p className='boldText'>{avgScore}</p>
+                <span>
+                  <FaStar color='#fff' size={iconSize}/>
+                  {avgQuality}
+                </span>
+              </div>
+              <div className={style.averageGroup}>
+                <AverageReviews title='Services' score={services}/>
+                <AverageReviews title='Locations' score={location}/>
+                <AverageReviews title='Ameneties' score={amenities}/>
+              </div>
+              <div className={style.averageGroup}>
+                <AverageReviews title='Prices' score={prices}/>
+                <AverageReviews title='Food' score={food}/>
+                <AverageReviews title='Room confort and quality' score={quality}/>
+              </div>
+            </div>
 
             <form onSubmit={mostraReview}>
               <h3>Add a review</h3>
@@ -62,7 +100,6 @@ const TuorView = () => {
 
               <FormButton title='Submit Review' type='submit' />
             </form>
-
 
           </aside>
           <aside className={style.priceBox}>

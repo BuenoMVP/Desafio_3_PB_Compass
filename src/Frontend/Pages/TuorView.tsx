ac5d80b34dbb932128bb0c2c317d4c9c92bfc504
Marcos Bueno <marcos_vbp@outlook.com>
2024-08-26 14:28:54 -0300

    feat: add bd connection

--- src/Frontend/Pages/TuorView.tsx
+++ src/Frontend/Pages/TuorView.tsx
@@ -7,21 +7,23 @@ import global from './global.module.css'
 import style from './tuorView.module.css'
 import AverageReviews from '../Components/AverageReviews'
 import { FaStar } from 'react-icons/fa'
+import api from '../Services/api'
 
 const TuorView = () => {
-  const [services, setServices] = useState<number>(5)
-  const [location, setLocation] = useState<number>(5)
-  const [amenities, setAmenities] = useState<number>(5)
-  const [prices, setPrices] = useState<number>(4)
-  const [food, setFood] = useState<number>(3)
-  const [quality, setQuality] = useState<number>(5)
+  const [nota_service, setServices] = useState<number>(1)
+  const [nota_location, setLocation] = useState<number>(1)
+  const [nota_amenities, setAmenities] = useState<number>(1)
+  const [nota_prices, setPrices] = useState<number>(1)
+  const [nota_food, setFood] = useState<number>(1)
+  const [nota_confort, setConfort] = useState<number>(1)
   const [name, setName] = useState<string>('anonymous')
-  const [email, setEmail] = useState<string>('')
-  const [comment, setComment] = useState<string>('No comment.')
+  const [email, setEmail] = useState<string>('standart@standart.com')
+  const [description, setDescription] = useState<string>('No comment.')
+  const tuorID: string = '66c7887d5277fe78a9645c0e'
 
   const lat: number = -23.187049672867087
   const lng: number = -50.65637960242221
-  const avgScore: number = (services + location + amenities + prices + quality) / 5
+  const avgScore: number = (nota_service + nota_location + nota_amenities + nota_prices + nota_confort + nota_food) / 6
   const iconSize: number = 20
   let avgQuality: string
 
@@ -33,9 +35,28 @@ const TuorView = () => {
     avgQuality = 'Medium'
   }
 
-  function mostraReview (e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    console.log('Nome: '+name, ' -- email: '+email, '-- Comentario: '+comment)
+  const handleForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    try {
+      const newReview = {
+        description,
+        email,
+        name,
+        nota_service,
+        nota_location,
+        nota_amenities,
+        nota_prices,
+        nota_confort,
+        nota_food,
+        tuorID
+      }
+
+      await api.post("/reviews", newReview)
+      console.log('fez o stringify')
+    } catch (err) {
+      console.log('Deu ruim no formulario: '+err)
+    }
   }
   
   return (
@@ -44,7 +65,7 @@ const TuorView = () => {
         <section className={`${global.contentSection} ${global.lastContentSection} ${style.section}`}>
           <aside className={style.tuorBox}>
             Informaçoes do tuor
-            <h2 className={`blueText`} >Overview</h2>
+            <h2 className={`blueText`}>Overview</h2>
             <span>Istanbul, the vibrant and historic city straddling the continents of Europe and Asia, 
               offers an enchanting blend of cultures, sights, and experiences that captivate every traveler’s heart.
                As Turkey’s cultural and economic hub. Instabul seamlesly fuses its rich heritage with modernity, 
@@ -63,18 +84,18 @@ const TuorView = () => {
                 </span>
               </div>
               <div className={style.averageGroup}>
-                <AverageReviews title='Services' score={services}/>
-                <AverageReviews title='Locations' score={location}/>
-                <AverageReviews title='Ameneties' score={amenities}/>
+                <AverageReviews title='Services' score={nota_service}/>
+                <AverageReviews title='Locations' score={nota_location}/>
+                <AverageReviews title='Ameneties' score={nota_amenities}/>
               </div>
               <div className={style.averageGroup}>
-                <AverageReviews title='Prices' score={prices}/>
-                <AverageReviews title='Food' score={food}/>
-                <AverageReviews title='Room confort and quality' score={quality}/>
+                <AverageReviews title='Prices' score={nota_prices}/>
+                <AverageReviews title='Food' score={nota_food}/>
+                <AverageReviews title='Room confort and quality' score={nota_confort}/>
               </div>
             </div>
 
-            <form onSubmit={mostraReview}>
+            <form onSubmit={handleForm}>
               <h3>Add a review</h3>
               
               <div>
@@ -95,7 +116,7 @@ const TuorView = () => {
               <textarea 
                 placeholder='Write your comment.' 
                 name='comment'
-                onChange={(e) => setComment(e.target.value)}
+                onChange={(e) => setDescription(e.target.value)}
               />
 
               <FormButton title='Submit Review' type='submit' />

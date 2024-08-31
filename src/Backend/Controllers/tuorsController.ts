d2947a18375da25596ff57b56e6cea3b113397cc
Marcos Bueno <marcos_vbp@outlook.com>
2024-08-31 17:52:23 -0300

    fix: fix BD types and controllers

--- src/Backend/Controllers/tuorsController.ts
+++ src/Backend/Controllers/tuorsController.ts
@@ -1,7 +1,8 @@
 import schemaTuors from "../Models/Tuors";
 import { Request, Response } from "express";
-import { tuorsProps } from "../Types/bdTypes";
+import { averageProps, tuorsProps } from "../Types/bdTypes";
 import averageController from "./averageController";
+import schemaAverage from "../Models/Average";
 
 const tuorsController = {
     postTuors: async (req: Request, res: Response) => {
@@ -14,14 +15,26 @@ const tuorsController = {
                 price_person: req.body.price_person,
                 time: req.body.time,
                 max_person: req.body.max_person,
-                min_age: req.body.min_age
+                min_age: req.body.min_age,
+                reviews: {
+                    avg_service: 0,
+                    avg_location: 0,
+                    avg_amenities: 0,
+                    avg_prices: 0,
+                    avg_confort: 0,
+                    avg_food: 0,
+                    avg_average: 0,
+                    qtdReviews: 0,
+                    tuorID: 'null',
+                    allReviews: []
+                }
             }
 
             const objTuor = await schemaTuors.create(newTuor)
 
             const _tuorID = objTuor._id
 
-            const _averageID = await averageController.postAverage(_tuorID.toString())
+            const _averageID: averageProps | undefined = await averageController.postAverage(_tuorID.toString())
 
             newTuor = {
                 title: req.body.title,
@@ -32,10 +45,10 @@ const tuorsController = {
                 time: req.body.time,
                 max_person: req.body.max_person,
                 min_age: req.body.min_age,
-                reviews: _averageID
+                reviews: _averageID!
             }
 
-            const newObjTuor = await schemaTuors.findOneAndUpdate(_tuorID, newTuor)
+            const newObjTuor = await schemaTuors.findOneAndUpdate({_id: _tuorID}, newTuor)
             
             res.status(201).json({newObjTuor, msg: 'Tuor created!'})
             
@@ -44,28 +57,36 @@ const tuorsController = {
         }
     },
     getAllTuors: async (req: Request, res: Response) => {
-        const limit:number = Number(req.query.limit) | 9
+        const limit:number = 9
         const offset:number = Number(req.query.offset) | 0
         
         try {
-            const objTuor = await schemaTuors.find().skip(offset).limit(limit).populate('reviews')
-            const total:number = await schemaTuors.countDocuments()
-            const url:string = req.baseUrl
+            const objTuor: tuorsProps[] = await schemaTuors.find().skip(offset).limit(limit).populate('reviews')
 
-            const next:number = offset + limit
-            const nextURL:string = next < total ? `${url}?limit=${limit}&offset=&${next}` : 'null'
+            const objReviews: averageProps[] = await schemaAverage.find().skip(offset).limit(limit)
 
-            const previous:number = offset - limit < 0 ? -1 : offset - limit
-            const previousURL: string = previous != -1 ? `${url}?limit=${limit}&offset=&${previous}` : 'null'
-
-            res.status(200).send({
-                nextURL,
-                previousURL,
-                limit,
-                offset,
-                total,
-                objTuor
-            })
+            if (!objTuor || objTuor.length == 0) {
+                res.status(404).send({Error: 'Tuors not found!'})
+            } else {
+                
+                const total:number = await schemaTuors.countDocuments()
+                const url:string = req.baseUrl
+    
+                const next:number = offset + limit
+                const nextURL:string = next < total ? `${url}?limit=${limit}&offset=&${next}` : 'null'
+    
+                const previous:number = offset - limit < 0 ? -1 : offset - limit
+                const previousURL: string = previous != -1 ? `${url}?limit=${limit}&offset=&${previous}` : 'null'
+    
+                res.status(200).send({
+                    nextURL,
+                    previousURL,
+                    offset,
+                    total,
+                    objTuor,
+                    objReviews
+                })
+            }
         } catch (err) {
             res.status(400).json({"Error to GET Tuors": err})
         }
@@ -88,6 +109,8 @@ const tuorsController = {
     updateTuors: async (req: Request, res: Response) => {
         try {
             const { id } = req.params
+            const tuorReview: averageProps | null = await schemaAverage.findOne({tuorID: {id}})
+
             const updateTuor: tuorsProps = {
                 title: req.body.title,
                 overview: req.body.overview,
@@ -96,7 +119,8 @@ const tuorsController = {
                 price_person: req.body.price_person,
                 time: req.body.time,
                 max_person: req.body.max_person,
-                min_age: req.body.min_age
+                min_age: req.body.min_age,
+                reviews: tuorReview!
             } 
 
             const objTuor = await schemaTuors.findByIdAndUpdate(id, updateTuor)

3b4de16409c1c49d58b0041c6ef8665cd350f5f2
Marcos Bueno <marcos_vbp@outlook.com>
2024-09-01 23:38:47 -0300

    feat: add email and password validation

--- src/Frontend/Pages/Login.tsx
+++ src/Frontend/Pages/Login.tsx
@@ -5,19 +5,51 @@ import Header from '../Components/Header'
 import style from './login.module.css'
 import { FaFacebook, FaGoogle } from 'react-icons/fa'
 
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup, FacebookAuthProvider } from 'firebase/auth'
 import { auth } from '../Services/firebase'
 
 const Login = () => {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [validEmail, setValidEmail] = useState<boolean>(true);
+    const [validPassword, setValidPassword] = useState<boolean>(true);
 
+    const regex: RegExp = /^[^\s@]+@[^\s@]+\.(com|br)$/
+    
     const iconSize: number = 20
 
-    function handleGoogle () {
-        const provider = new GoogleAuthProvider()
+    function validateEmail (value: string) {
+        setEmail(value)
+        if (regex.test(value)) {
+            setValidEmail(true)
+        } else {
+            setValidEmail(false)
+        }
+    }
 
-        signInWithPopup(auth, provider)
+    function validatePassword (value: string) {
+        setPassword(value)
+        if (password.length >= 5) {
+            setValidPassword(true)
+        } else {
+            setValidPassword(false)
+        }
+    }
+
+    function handleGoogle () {
+        const googleProvider = new GoogleAuthProvider()
+
+        signInWithPopup(auth, googleProvider)
+            .then(res => console.log(res))
+            .catch(err =>console.log(err))
+    }
+
+    function handleFacebook () {
+        const faceProvider = new FacebookAuthProvider()
+
+        console.log(faceProvider)
+
+        signInWithPopup(auth, faceProvider)
             .then(res => console.log(res))
             .catch(err =>console.log(err))
     }
@@ -26,20 +58,27 @@ const Login = () => {
     <main className={style.pageContent}>
         <Header />
         <section className={`${style.section}`}>
+            <div className={`${style.apresentation} blueText`}>
+                <img src="https://desafio-3-mvbp-bucket.s3.amazonaws.com/Logo_Black.svg" alt="logo-image" />   
+                <h1>Your journey starts here. <p className='redText'>Let's explore the world together.</p></h1>
+                <h2>Ready to plan your next adventure?</h2>
+            </div>
 
             <div className={style.formLogin}>
-                <h1>Wellcome!</h1>
+                <div className={style.texts}>
+                    <h1>Log in to find your perfect trip.</h1>
+                </div>
 
                 <form action="POST">
                     <div>
                         <label>Email</label>
-                        <input type="text" placeholder='Enter your email' onChange={(e) => setEmail(e.target.value)}/>
-                        {!email && <p>Invalid Email</p>}
+                        <input type="text" placeholder='Enter your email' onChange={(e) => validateEmail(e.target.value)}/>
+                        {!validEmail && <p className={style.errorText}>Invalid Email</p>}
                     </div>
                     <div>
                         <label>Password</label>
-                        <input type="password" placeholder='Enter your passowrd' onChange={(e) => setPassword(e.target.value)}/>
-                        {password}
+                        <input type="password" placeholder='Enter your passowrd' onChange={(e) => validatePassword(e.target.value)}/>
+                        {!validPassword && <p className={style.errorText}>Min 6 characters</p>}
                     </div>
                     <FormButton title='Login' type='submit' />
                 </form>
@@ -49,8 +88,8 @@ const Login = () => {
                         <FaGoogle size={iconSize}/>
                         Google
                     </button>
-                    <button id={style.facebook}>
-                        <FaFacebook size={iconSize} />
+                    <button id={style.facebook} onClick={handleFacebook}>
+                        <FaFacebook size={iconSize}  />
                         Facebook
                     </button>
                 </div>
